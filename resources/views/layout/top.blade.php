2379ce1b81ca05361220bdcc10cd552a154137dc
Alwoku <pandochka55505@gmail.com>
2022-11-07 15:42:20 +0300

    my project in Laravel

--- resources/views/layout/top.blade.php
+++ resources/views/layout/top.blade.php
@@ -3,18 +3,16 @@
 @section('titel')TOP10 @endsection
 @section('header1') <a href="{{ route('home')}}">Главная</a> @endsection
 @section('content')
-<div class="container is-fullwidth is-normal">
-    <div class="title is-3">
+<div class="card-footer">
         <div class="blok">
-             <a href="{{route('fast')}}" class = "link m-5 ">TOP10 самых быстрых</a>
+             <a href="{{route('fast')}}" class = "card-footer-item">TOP10 самых быстрых</a>
         </div>
         <div class="blok">
-            <a href="{{route('errors')}}"  class = "link m-5 ">TOP10 ошибок</a>
+            <a href="{{route('errors')}}"  class = "card-footer-item ">TOP10 ошибок</a>
         </div>
         <div class="blok">  
-            <a href="{{route('month')}}"  class = "link m-5 ">Таблица месяц общая</a>
+            <a href="{{route('month')}}"  class = "card-footer-item">Таблица месяц общая</a>
         </div>  
-    </div>
 </div>
 
 @yield('top')
